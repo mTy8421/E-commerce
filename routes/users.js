9fffb5f463870afb780217a0498f4e1f85ea04c2
Panthakit totid <koontyza007@gmail.com>
2023-10-28 23:16:29 +0700

    update controllers

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,7 @@
 var express = require("express");
 var router = express.Router();
+var controller = require("../controllers/userControllers");
 
-/* GET users listing. */
-router.get("/", function (req, res, next) {
-  res.send("respond with a resource");
-});
+router.get("/", controller.index);
 
 module.exports = router;
