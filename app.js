8a00f107002169893317c61e71677a4021bccc56
Panthakit totid <koontyza007@gmail.com>
2023-11-04 23:15:14 +0700

    edit static

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
-app.use("/dist", express.static(__dirname, "node_modules", "dist"));
+// app.use("/dist", express.static(__dirname, "node_modules", "dist"));
 
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
